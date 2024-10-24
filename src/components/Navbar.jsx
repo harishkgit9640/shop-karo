3d3b43efcfcd6ba4941f5ed2fd1a832ab8960fad
Harish Kumar <harishstudio877@gmail.com>
2024-10-24 08:34:11 +0530

    navigation for mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom';
 import BRAND_LOGO from '../assets/img/logo.png';
 
 const Navbar = () => {
+
+    const [showHide, setShowHide] = useState(true)
+
     return (
         <section id="header">
             <div className="brand">
                 <NavLink to=""><img src={BRAND_LOGO} alt="logo" className="logo" /></NavLink>
             </div>
             <div className="menu">
-                <ul id="navbar">
+                <ul id="navbar" className={!showHide ? 'active' : ''}>
                     <li><NavLink to="/">Home</NavLink></li>
                     <li><NavLink to="/shop">Shop</NavLink></li>
                     <li><NavLink to="/blog">Blog</NavLink></li>
                     <li><NavLink to="/about">About</NavLink></li>
                     <li><NavLink to="/contact">Contact</NavLink></li>
                     <li className="lg_bag"><NavLink to="/cart"><i className="fa-solid fa-bag-shopping"></i></NavLink></li>
-                    <NavLink to="#"><i className="fas fa-times" id="close"></i></NavLink>
                 </ul>
             </div>
             <div className="menu_btn">
                 <NavLink to="/cart"><i className="fa-solid fa-bag-shopping"></i></NavLink>
-                <i className="fas fa-outdent" id="bar"></i>
+                <i onClick={() => setShowHide(!showHide)} className={showHide ? "fas fa-outdent" : "fas fa-times"} id="bar"></i>
             </div>
         </section>
     )
